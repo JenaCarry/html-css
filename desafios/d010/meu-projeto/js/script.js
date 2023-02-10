37102a1a1a40239ebb4a15db807905ef358a3ad4
Jean <120676537+JenaCarry@users.noreply.github.com>
2023-02-10 02:07:08 -0300

    ae

--- desafios/d010/meu-projeto/js/script.js
+++ desafios/d010/meu-projeto/js/script.js
@@ -1,11 +1,18 @@
 let nav = document.querySelector("#nav");
 
+function mudou() {
+  if (innerWidth >= 600) {
+    nav.classList.remove("open");
+    document.querySelector(".menu").src = "imagens/menu_white.svg";
+  }
+}
+
 function clicar() {
   if (nav.classList.contains("open")) {
     nav.classList.remove("open");
-    document.querySelector(".menu").src = "imagens/menu_white.svg"
+    document.querySelector(".menu").src = "imagens/menu_white.svg";
   } else {
     nav.classList.add("open");
-    document.querySelector(".menu").src = "imagens/close_white.svg"
+    document.querySelector(".menu").src = "imagens/close_white.svg";
   }
 }
